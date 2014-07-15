db3a0f99569d80493fa09128baf87bac51be54c0
RomainStage <romain.cunault@icloud.com>
2014-07-15 10:31:24 +0200

    k

--- server.js
+++ server.js
@@ -4,29 +4,7 @@ var util = require("util");
 //var charts3 = require("./dataIDF.js");
 
 //charts3.fetch_info();
-function startKeepAlive() {
-    setInterval(function() {
-        var options = {
-            host: 'http://shielded-taiga-6506.herokuapp.com',
-           // port: 80,
-            path: '/'
-        };
-        http.get(options, function(res) {
-            res.on('data', function(chunk) {
-                try {
-                    // optional logging... disable after it's working
-                    console.log("HEROKU RESPONSE: " + chunk);
-                } catch (err) {
-                    console.log(err.message);
-                }
-            });
-        }).on('error', function(err) {
-            console.log("Error: " + err.message);
-        });
-    }, 20 * 60 * 1000); // load every 20 minutes
-}
 
-startKeepAlive();
 
 
 var server = {}; //Server object. This object is use to stock everything owned by the server.
